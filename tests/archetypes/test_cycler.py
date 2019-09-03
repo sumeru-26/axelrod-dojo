5712c6686576331f0741c6818e0c7a2787a3150f
Marc Harper <marc.harper@gmail.com>
2019-09-02 23:32:27 -0700

    Update test

--- tests/archetypes/test_cycler.py
+++ tests/archetypes/test_cycler.py
@@ -16,7 +16,7 @@ class TestCyclerParams(unittest.TestCase):
         axl.seed(0)
         test_length = 10
         self.instance = CyclerParams(sequence_length=test_length)
-        self.assertEqual(self.instance.sequence, [D, C, C, D, C, C, C, C, C, C])
+        self.assertEqual(self.instance.sequence, [C, D, D, C, D, D, D, D, D, D])
         self.assertEqual(self.instance.sequence_length, test_length)
         self.assertEqual(len(self.instance.sequence), test_length)
 
